dd22d4a6dece063d13cc0a72c1d861588a306bb4
Hiroshi Yoshimura <hyoshimura@gmail.com>
2024-03-14 14:58:06 +0700

    x:telescope o:fzf

--- init.lua
+++ init.lua
@@ -32,60 +32,17 @@ require('packer').startup(function(use)
 	-- multi comment
 	use 'tpope/vim-commentary'
 
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.5',
-		requires = {
-      { "nvim-telescope/telescope-live-grep-args.nvim" },
-      {'nvim-lua/plenary.nvim'},
-    },
-    config = function()
-      require("telescope").load_extension("live_grep_args")
-    end
-	}
-
-	-- fzf
-	-- use { 'junegunn/fzf', run = ":call fzf#install()" }
-	-- use { 'junegunn/fzf.vim' }
-
-	use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-
+-- fzf
+	use { 'junegunn/fzf', run = ":call fzf#install()" }
+	use { 'junegunn/fzf.vim' }
 
 end)
+
 require("mason").setup()
+
 -- Set up nvim-cmp.
 local cmp = require'cmp'
-
-cmp.setup({
---   snippet = {
---     -- REQUIRED - you must specify a snippet engine
---     expand = function(args)
---       vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
---       -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
---       -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
---       -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
---     end,
---   },
---   window = {
---     -- completion = cmp.config.window.bordered(),
---     -- documentation = cmp.config.window.bordered(),
---   },
---   mapping = cmp.mapping.preset.insert({
---     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
---     ['<C-f>'] = cmp.mapping.scroll_docs(4),
---     ['<C-Space>'] = cmp.mapping.complete(),
---     ['<C-e>'] = cmp.mapping.abort(),
---     ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
---   }),
---   sources = cmp.config.sources({
---     { name = 'nvim_lsp' },
---     { name = 'vsnip' }, -- For vsnip users.
---     -- { name = 'luasnip' }, -- For luasnip users.
---     -- { name = 'ultisnips' }, -- For ultisnips users.
---     -- { name = 'snippy' }, -- For snippy users.
---   }, {
---     { name = 'buffer' },
---   })
-})
+cmp.setup({ })
 
 -- Set configuration for specific filetype.
 cmp.setup.filetype('gitcommit', {
@@ -94,14 +51,6 @@ cmp.setup.filetype('gitcommit', {
   }, {
     { name = 'buffer' },
   })
-})
-
--- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline({ '/', '?' }, {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = {
-    { name = 'buffer' }
-  }
 })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
@@ -128,36 +77,10 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 require("nvim-tree").setup()
 
-local telescope = require("telescope")
-local lga_actions = require("telescope-live-grep-args.actions")
-
-require('telescope').setup{
-	extentions = {
-		fzf = {
-			fuzzy = true,                    -- false will only do exact matching
-			override_generic_sorter = true, -- override the generic sorter
-			override_file_sorter = true,     -- override the file sorter
-			case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-			-- the default case_mode is "smart_case"
-		},
-		live_grep_args = {
-			auto_quoting = true, -- enable/disable auto-quoting
-			-- define mappings, e.g.
-			mappings = { -- extend mappings
-			i = {
-				["<C-k>"] = lga_actions.quote_prompt(),
-				["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
-			},
-		},
-		-- ... also accepts theme settings, for example:
-		-- theme = "dropdown", -- use dropdown theme
-		-- theme = { }, -- use own theme spec
-		-- layout_config = { mirror=true }, -- mirror preview pane
-	}
-}
-}
-require('telescope').load_extension('fzf')
-
+-- fzf settings
+vim.env.FZF_DEFAULT_OPTS = "--layout=reverse"
+vim.env.FZF_jEFAULT_COMMAND = "rg --files --hidden --glob '!.git/**'"
+vim.g.fzf_layout = { window = { width = 1, height = 0.5, yoffset = 1, xoffset = 0.5, border = 'sharp' } }
 
 -- +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 --                                 keymap
@@ -174,27 +97,17 @@ vim.o.clipboard = "unnamedplus"
 -- スペースキーを <leader> に設定する
 vim.api.nvim_set_keymap('n', '<Space>', '<Nop>', {noremap = true, silent = true})
 vim.g.mapleader = ' '
+
 -- スペースキーが押されたときに <leader> を実行する
 vim.api.nvim_set_keymap('n', '<Leader>', '<Space>', {noremap = true, silent = true})
 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>f', builtin.find_files, {})
-vim.keymap.set('n', '<leader>g', builtin.git_files, {})
-vim.keymap.set('n', '<leader>b', builtin.buffers, {})
-vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
--- vim.keymap.set('n', '<leader>l', builtin.live_grep, {})
-vim.keymap.set("n", "<leader>l", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
+-- nvim-tree setting
+vim.keymap.set('n', '<leader>l', ':NvimTreeToggle<CR>', {})
 
-vim.api.nvim_set_keymap('n', '<F3>', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
-
--- function _G.fzf_files()
---   vim.fn['fzf#run']({
---     source = 'rg --files',
---     sink = 'e',
---     options = '--height 30% --prompt "Files> " --multi --preview "bat --color=always {}"'
---   })
--- end
-
--- <leader>f にキーバインドを設定
--- vim.api.nvim_set_keymap('n', '<leader>f', '<cmd>lua fzf_files()<CR>', {noremap = true, silent = true})
--- vim.api.nvim_set_keymap('n', '<leader>f', ':lua fzf_files()<CR>', {noremap = true, silent = true})
+-- fzf setting
+vim.keymap.set('n', '<leader>f', ':Files<CR>', {})
+vim.keymap.set('n', '<leader>g', ':GFiles?<CR>', {})
+vim.keymap.set('n', '<leader>G', ':GFiles<CR>', {})
+vim.keymap.set('n', '<leader>b', ':Buffers<CR>', {})
+vim.keymap.set('n', '<leader>h', ':History<CR>', {})
+vim.keymap.set('n', '<leader>r', ':Rg<CR>', {})
